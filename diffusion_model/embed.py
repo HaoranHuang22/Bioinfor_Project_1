9035e7295b9e440e4113dba213d948f805c36abc
HaoranHuang <1328463108@qq.com>
2023-03-20 17:22:53 +0100

    delete get_secondary_representaion function

--- diffusion_model/embed.py
+++ diffusion_model/embed.py
@@ -8,8 +8,8 @@ def get_single_representation(pdb_chain, res_label):
     Function to get single representation
 
     Args:
-        pdb_chain(tuple): batch_size
-        res_label(torch.Tensor): dim -> (batch_size, num_res, 1)
+        pdb_chain(tuple): a list of pdb chains
+        res_label(np.array): dim -> (batch_size, num_res, 1)
 
     Returns:
         single_repr(torch.Tensor): dim -> (batch_size, embedding_dim)
@@ -35,28 +35,12 @@ def get_single_representation(pdb_chain, res_label):
     with torch.no_grad():
         results = model(batch_tokens, repr_layers=[33], return_contacts=True)
     token_representations = results["representations"][33]
-
-    # Generate per-sequence representations via averaging
+    
+    # Generate per-sequence representations
     # NOTE: token 0 is always a beginning-of-sequence token, so the first residue is token 1.
     sequence_representations = []
     for i, tokens_len in enumerate(batch_lens):
-        sequence_representations.append(token_representations[i, 1 : tokens_len - 1].mean(0))
+        sequence_representations.append(token_representations[i, 1 : tokens_len - 1])
     
     single_repr = torch.stack(sequence_representations, dim=0)
     return single_repr
-
-def get_secondary_representation(atom_coords: torch.Tensor):
-    """
-    Function to get pair representation, C alpha atom distance map
-
-    Args:
-        atom_coords(torch.Tensor): dim -> (batch_size, num_res, 3, 3)
-
-    Returns:
-        pair_repr(torch.Tensor): dim -> (batch_size, num_res, num_res)
-    """
-    ca_coords = atom_coords[:,:,1]
-
-    # Compute the distance matrix
-    dist_mat = torch.cdist(ca_coords, ca_coords, p=2)
-    return dist_mat
